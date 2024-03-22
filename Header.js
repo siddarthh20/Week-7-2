ebdb97d64d5474133cb2e1a5ab52861f6bbed02b
siddarthh20 <155608049+siddarthh20@users.noreply.github.com>
2024-03-22 21:15:49 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,18 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Travel Explorer</h1>
+      <nav>
+        <ul>
+          <li><a href="#destinations">Destinations</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
