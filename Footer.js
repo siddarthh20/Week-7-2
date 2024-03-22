14f79dd9676dbe8aa03687a8193ba25d2a59675c
siddarthh20 <155608049+siddarthh20@users.noreply.github.com>
2024-03-22 21:15:22 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,24 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h3>Contact Information</h3>
+        <p>Email: PACKYOURBAGS@travelexplorer.com</p>
+        <p>Phone: +1234567890</p>
+      </div>
+      <div>
+        <h3>Follow Us</h3>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
