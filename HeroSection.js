7e696f4cae042b0662d9c484ad6ec82899c1bbc8
siddarthh20 <155608049+siddarthh20@users.noreply.github.com>
2024-03-22 21:16:15 +0530

    Create HeroSection.js

---
+++ HeroSection.js
@@ -0,0 +1,16 @@
+// HeroSection.js
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <section id="hero">
+      <img src="https://img.freepik.com/free-vector/realistic-travel-background-with-elements_52683-77784.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710979200&semt=ais" alt="Travel" />
+      <div>
+        <h2>Welcome to Travel Explorer</h2>
+        <p>Explore the world's most amazing destinations with us.</p>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
